002f1e879bbf93ae2323ade623313329ab85774e
holdingcircles <100821064+holdingcircles@users.noreply.github.com>
2022-05-26 18:31:03 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,259 @@
+<?php
+
+// A collection of sample products
+$products = json_decode(file_get_contents('Data/data.json'));
+$colors = [
+	'black'      => 'Black',
+	'space-gray' => 'Space Gray',
+	'jet-black'  => 'Jet Black',
+	'silver'     => 'Silver',
+	'gold'       => 'Gold',
+	'rose-gold'  => 'Rose Gold',
+];
+
+// Page
+$a = (isset($_GET['a'])) ? $_GET['a'] : 'home';
+
+require_once 'Model/class.Cart.php';
+
+// Initialize cart object
+$cart = new Cart([
+	// Maximum item can added to cart, 0 = Unlimited
+	'cartMaxItem' => 0,
+
+	// Maximum quantity of a item can be added to cart, 0 = Unlimited
+	'itemMaxQuantity' => 5,
+
+	// Do not use cookie, cart items will gone after browser closed
+	'useCookie' => false,
+]);
+
+// Shopping Cart Page
+if ($a == 'cart') {
+	$cartContents = '
+	<div class="alert alert-warning">
+		<i class="fa fa-info-circle"></i> There are no items in the cart.
+	</div>';
+
+	// Empty the cart
+	if (isset($_POST['empty'])) {
+		$cart->clear();
+	}
+
+	// Add item
+	if (isset($_POST['add'])) {
+		foreach ($products as $product) {
+			if ($_POST['id'] == $product->id) {
+				break;
+			}
+		}
+
+		$cart->add($product->id, $_POST['qty'], [
+			'price' => $product->price,
+			'color' => (isset($_POST['color'])) ? $_POST['color'] : '',
+		]);
+	}
+
+	// Update item
+	if (isset($_POST['update'])) {
+		foreach ($products as $product) {
+			if ($_POST['id'] == $product->id) {
+				break;
+			}
+		}
+
+		$cart->update($product->id, $_POST['qty'], [
+			'price' => $product->price,
+			'color' => (isset($_POST['color'])) ? $_POST['color'] : '',
+		]);
+	}
+
+	// Remove item
+	if (isset($_POST['remove'])) {
+		foreach ($products as $product) {
+			if ($_POST['id'] == $product->id) {
+				break;
+			}
+		}
+
+		$cart->remove($product->id, [
+			'price' => $product->price,
+			'color' => (isset($_POST['color'])) ? $_POST['color'] : '',
+		]);
+	}
+
+	if (!$cart->isEmpty()) {
+		$allItems = $cart->getItems();
+
+		$cartContents = '
+		<table class="table table-striped table-hover">
+			<thead>
+				<tr>
+					<th class="col-md-7">Product</th>
+					<th class="col-md-3 text-center">Quantity</th>
+					<th class="col-md-2 text-right">Price</th>
+				</tr>
+			</thead>
+			<tbody>';
+
+		foreach ($allItems as $id => $items) {
+			foreach ($items as $item) {
+				foreach ($products as $product) {
+					if ($id == $product->id) {
+						break;
+					}
+				}
+
+				$cartContents .= '
+				<tr>
+					<td>' . $product->name . ((isset($item['attributes']['color'])) ? ('<p><strong>Color: </strong>' . $colors[$item['attributes']['color']] . '</p>') : '') . '</td>
+					<td class="text-center"><div class="form-group"><input type="number" value="' . $item['quantity'] . '" class="form-control quantity pull-left" style="width:100px"><div class="pull-right"><button class="btn btn-default btn-update" data-id="' . $id . '" data-color="' . ((isset($item['attributes']['color'])) ? $item['attributes']['color'] : '') . '"><i class="fa fa-refresh"></i> Update</button><button class="btn btn-danger btn-remove" data-id="' . $id . '" data-color="' . ((isset($item['attributes']['color'])) ? $item['attributes']['color'] : '') . '"><i class="fa fa-trash"></i></button></div></div></td>
+					<td class="text-right">$' . $item['attributes']['price'] . '</td>
+				</tr>';
+			}
+		}
+
+		$cartContents .= '
+			</tbody>
+		</table>
+
+		<div class="text-right">
+			<h3>Total:<br />$' . number_format($cart->getAttributeTotal('price'), 2, '.', ',') . '</h3>
+		</div>
+
+		<p>
+			<div class="pull-left">
+				<button class="btn btn-danger btn-empty-cart">Empty Cart</button>
+			</div>
+			<div class="pull-right text-right">
+				<a href="?a=home" class="btn btn-default">Continue Shopping</a>
+				<a href="?a=checkout" class="btn btn-danger">Checkout</a>
+			</div>
+		</p>';
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Shop - A Simple PHP SHOP</title>
+
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+		<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cosmo/bootstrap.min.css" rel="stylesheet">
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+		<style>
+			body{margin-top:50px;margin-bottom:200px}
+		</style>
+	</head>
+
+	<body>
+		<div class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<a href="?a=shop" class="navbar-brand">Shop V1</a>
+					<button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+
+				<div class="navbar-collapse collapse" id="navbar-main">
+					<ul class="nav navbar-nav">
+						<li><a href="?a=cart" id="li-cart"><i class="fa fa-shopping-cart"></i> Cart (<?php echo $cart->getTotalItem(); ?>)</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<?php if ($a == 'cart'): ?>
+		<div class="container">
+			<h1>Shopping Cart</h1>
+
+			<div class="row">
+				<div class="col-md-12">
+					 <div class="table-responsive">
+						<?php echo $cartContents; ?>
+					 </div>
+				</div>
+			</div>
+		</div>
+		<?php elseif ($a == 'checkout'): ?>
+		<div class="container">
+			<h1>Checkout</h1>
+
+			<div class="row">
+				<div class="col-md-12">
+					 <div class="table-responsive">
+					 	<pre><?php print_r($cart->getItems()); ?></pre>
+					 </div>
+				</div>
+			</div>
+		</div>
+		<?php else: ?>
+		<div class="container">
+			<h1>Products</h1>
+			<div class="row">
+				<?php
+				foreach ($products as $product) {
+					echo '
+					<div class="col-md-6">
+						<h3>' . $product->name . '</h3>
+
+						<div>
+							<div class="pull-left">
+								<img src="' . $product->image->source . '" border="0" width="' . $product->image->width . '" height="' . $product->image->height . '" title="' . $product->name . '" />
+							</div>
+							<div class="pull-right">
+								<h4>$' . $product->price . '</h4>
+								<form>
+									<input type="hidden" value="' . $product->id . '" class="product-id" />';
+
+					if ($product->colors) {
+						echo '
+										<div class="form-group">
+											<label>Color:</label>
+											<select class="form-control color">';
+
+						foreach ($product->colors as $key => $value) {
+							echo '
+												<option value="' . $key . '"> ' . $value . '</option>';
+						}
+
+						echo '
+											</select>
+										</div>';
+					}
+
+					echo '
+
+									<div class="form-group">
+										<label>Quantity:</label>
+										<input type="number" value="1" class="form-control quantity" />
+									</div>
+									<div class="form-group">
+										<button class="btn btn-danger add-to-cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+									</div>
+								</form>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+					</div>';
+				}
+				?>
+			</div>
+		</div>
+		<?php endif; ?>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+		<script src="public/js/script.js"></script>
+	</body>
+</html>
